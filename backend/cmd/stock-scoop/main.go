d681a9dd296494581b2a20e079bfe4eb4b1e3280
Prathamesh Khochade <p.khochade@somaiya.edu>
2025-07-20 11:37:30 +0530

    formated the project

---
+++ backend/cmd/stock-scoop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/bulbasor1509/stock-scoop/backend/handlers"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+
+	router := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+
+	router.GET("/ping", handlers.BulkHandler)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("failed to start the server on port 8080")
+	}
+}
---
+++ backend/handlers/bulk.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.com/bulbasor1509/stock-scoop/backend/crawler"
+	"github.com/bulbasor1509/stock-scoop/backend/response"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func BulkHandler(ctx *gin.Context) {
+	bulk, err := crawler.BulkCrawler("https://www.nseindia.com/report-detail/display-bulk-and-block-deals")
+
+	if err != nil {
+		response.Fail(ctx, http.StatusInternalServerError, err)
+	}
+	response.Success(ctx, http.StatusOK, bulk)
+}
